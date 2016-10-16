26c9b0bb150bf4b78efb44254b6ddcf15fbe6683
Ahmed Karic <ahmeddd_karic@hotmail.com>
2016-10-16 11:11:19 +0200

    lang-engineering-CS set

--- src/Knowlead.DTO/LookupModels/Core/FOSModel.cs
+++ src/Knowlead.DTO/LookupModels/Core/FOSModel.cs
@@ -4,9 +4,6 @@ namespace Knowlead.DTO.LookupModels.Core
 {
     public class FOSModel : _CoreLookupModel
     {
-        [MyRequired]
-        public string FosDesc { get; set; }
-
         public FOSModel ParentFos { get; set; }
         
         public int? ParentFosId { get; set; }
--- src/Knowlead.DomainModel/LookupModels/Core/FOS.cs
+++ src/Knowlead.DomainModel/LookupModels/Core/FOS.cs
@@ -4,9 +4,6 @@ namespace Knowlead.DomainModel.LookupModels.Core
 {
     public class FOS : _CoreLookup
     {
-        [MyRequired]
-        public string FosDesc { get; set; }
-
         public FOS ParentFos { get; set; }
 
         public int? ParentFosId { get; set; }
