9a2723d1b81126f51d66c7093ef7498567776342
Haris Botic <haris.botic96@gmail.com>
2016-10-29 21:51:21 +0200

    FOS DTO small update

--- src/Knowlead.DTO/LookupModels/Core/FOSModel.cs
+++ src/Knowlead.DTO/LookupModels/Core/FOSModel.cs
@@ -1,9 +1,11 @@
+using System.Collections.Generic;
+
 namespace Knowlead.DTO.LookupModels.Core
 {
     public class FOSModel : _CoreLookupModel
     {
-        public FOSModel ParentFos { get; set; }
-        
+        // public FOSModel ParentFos { get; set; }  removed for DTO only
         public int? ParentFosId { get; set; }
+        public ICollection<FOSModel> Children;
     }
 }
