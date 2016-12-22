bb5c28a4f1384a808e185d5e561eddaaa6a79cd7
Haris Botic <haris.botic96@gmail.com>
2016-12-22 17:46:03 +0100

    ApplicationUserRelationship DTO

---
+++ src/Knowlead.DTO/UserModels/ApplicationUserRelationshipModel.cs
@@ -0,0 +1,34 @@
+using System;
+using Knowlead.Common.DataAnnotations;
+
+namespace Knowlead.DTO.UserModels
+{
+    public class ApplicationUserRelationshipModel
+    {
+        [MyRequired]
+        public Guid ApplicationUserSmallerId { get; set; }
+        public ApplicationUserModel ApplicationUserSmaller { get; set; }
+
+        [MyRequired]
+        public Guid ApplicationUserBiggerId { get; set; }
+        public ApplicationUserModel ApplicationUserBigger { get; set; }
+
+        [MyRequired]
+        public UserRelationshipStatus Status { get; set; }
+
+        [MyRequired]
+        public Guid LastActionById { get; set; }
+        public ApplicationUserModel LastActionBy { get; set; }
+
+        public DateTime UpdatedAt { get; set; }
+        public DateTime CreatedAt { get; set; }
+
+        public enum UserRelationshipStatus 
+        {
+            Pending = 0,
+            Accepted = 1,
+            Declined = 2,
+            Blocked = 3
+        }
+    }
+}
