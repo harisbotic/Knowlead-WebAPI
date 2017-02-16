f82872d04573abf79563f0b2910bcfa626a7a175
Haris Botic <haris.botic96@gmail.com>
2017-02-17 00:25:56 +0100

    RewardModel update

--- src/Knowlead.DTO/LookupModels/Core/RewardModel.cs
+++ src/Knowlead.DTO/LookupModels/Core/RewardModel.cs
@@ -2,6 +2,7 @@ namespace Knowlead.DTO.LookupModels.Core
 {
     public class RewardModel : _CoreLookupModel
     {
-        
+        public int PointsReward { get; set; } = 0;
+        public int MinutesReward { get; set; } = 0;
     }
 }
