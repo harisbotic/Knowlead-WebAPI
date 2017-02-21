245af1d18ad679d505d0ebb8825ce4fdd7377e92
Haris Botic <haris.botic96@gmail.com>
2017-02-21 23:50:00 +0100

    Send feedbacks to email

--- src/Knowlead.BLL/Repositories/StatisticsRepository.cs
+++ src/Knowlead.BLL/Repositories/StatisticsRepository.cs
@@ -3,25 +3,30 @@ using System.Threading.Tasks;
 using Knowlead.BLL.Repositories.Interfaces;
 using Knowlead.DAL;
 using Knowlead.DomainModel.StatisticsModels;
+using Knowlead.Services;
 
 namespace Knowlead.BLL.Repositories
 {
     public class StatisticsRepository : IStatisticsRepository
     {
         private readonly ApplicationDbContext _context;
-        
-        public StatisticsRepository(ApplicationDbContext context)
+        private MessageServices _messageServices;
+
+        public StatisticsRepository(ApplicationDbContext context, MessageServices messageServices)
         {
             _context = context;
+            _messageServices = messageServices;
         }
 
-        public async Task<PlatformFeedback> SubmitPlatformFeedback(String feedback, Guid ApplicationUserId)
+        public async Task<PlatformFeedback> SubmitPlatformFeedback(String feedback, Guid applicationUserId)
         {
-            var platformFeedback = new PlatformFeedback() { Feedback = feedback, SubmittedById = ApplicationUserId };
+            var platformFeedback = new PlatformFeedback() { Feedback = feedback, SubmittedById = applicationUserId };
             
             _context.PlatformFeedbacks.Add(platformFeedback);
             await _context.SaveChangesAsync();
             
+            await _messageServices.TempSendEmailAsync("feedback@knowlead.co","New Platform Feedback", "haris.botic96@@gmail.com", applicationUserId.ToString(), feedback);
+
             return platformFeedback;
         }
     }
